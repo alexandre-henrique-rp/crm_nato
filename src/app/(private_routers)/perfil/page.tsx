9e54c6ee10e53688dd89b455c1618c9dfc11df60
Matheus Lopes <157651184+Brlfps@users.noreply.github.com>
2024-07-05 12:49:05 -0300

    Update page.tsx

--- src/app/(private_routers)/perfil/page.tsx
+++ src/app/(private_routers)/perfil/page.tsx
@@ -1,42 +1,22 @@
 "use client";
 
-import {
-    Box,
-    Text,
-    Input,
-    Flex,
-    Tabs,
-    TabList,
-    Tab,
-    Divider,
-    FormControl,
-    Stack,
-    SimpleGrid,
-    FormLabel,
-    GridItem,
-} from "@chakra-ui/react";
-import { useState } from "react";
-import { DadosPessoaisAdmComponent, DadosPessoaisComponent } from "./components/dados-pessoais";
+import { Flex } from "@chakra-ui/react";
+import { DadosPessoaisAdmComponent } from "./components/dados-pessoais";
 
 export default function perfilPage() {
-    
-    return (
-
+  return (
     <>
-        <Flex
-            alignItems="center"
-            justifyContent="space-evenly"
-            pt={10}
-            pb={10}
-            borderWidth={0}
-            overflowX="auto"
-            flexDir={"column"}   
-        >
-
-<DadosPessoaisAdmComponent />
-
-          
-        </Flex>
+      <Flex
+        alignItems="center"
+        justifyContent="space-evenly"
+        pt={10}
+        pb={10}
+        borderWidth={0}
+        overflowX="auto"
+        flexDir={"column"}
+      >
+        <DadosPessoaisAdmComponent />
+      </Flex>
     </>
-    );
+  );
 }
