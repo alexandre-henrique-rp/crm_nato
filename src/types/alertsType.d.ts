bddeab5d7865b4ae0a4f336fcb946c7bdb3aeea0
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2024-07-08 13:32:19 -0300

    update

---
+++ src/types/alertsType.d.ts
@@ -0,0 +1,30 @@
+declare module AlertsType {
+
+  /**
+   * AlertsProps
+   * @description Tipos de alertas
+   * @typedef {Object} AlertsProps
+   * @property {number} id - Id do alerta
+   * @property {string} titulo - Titulo do alerta
+   * @property {string} texto - Texto do alerta
+   * @property {number} solicitacao_id - Id da solicitacao
+   * @property {number} corretor - Id do corretor
+   * @property {string} tipo - Tipo do alerta
+   * @property {string} tag - Tag do alerta (Ex: "info" | "warning" | "success" | "error" | "loading")
+   * @property {number} empreendimento - Id do empreendimento
+   * @property {Date} createdAt - Data de criacao do alerta
+   * @property {Date} updatedAt - Data de atualizacao do alerta
+   */
+  export interface AlertsProps{
+    id: number;
+    titulo: string;
+    texto: string;
+    solicitacao_id: number;
+    corretor: number;
+    tipo: string;
+    tag: string;
+    empreendimento: number;
+    createdAt: Date | string |any;
+    updatedAt: Date | string | any | null;
+  }
+}
