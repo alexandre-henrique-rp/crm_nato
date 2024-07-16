2169b15265865698845db92c3f770e496f8bb623
Matheus Lopes <157651184+Brlfps@users.noreply.github.com>
2024-07-16 10:58:44 -0300

    10:58

--- src/app/(private_routers)/solicitacoes/[id]/components/dados-pessoais/index.tsx
+++ src/app/(private_routers)/solicitacoes/[id]/components/dados-pessoais/index.tsx
@@ -553,25 +553,6 @@ export const DadosPessoaisComponent = ({ SetData }: DadosPessoaisProps) => {
                       </>
                     );
                   })}
-                <Alert status="error">
-                  <AlertIcon />
-                  There was an error processing your request
-                </Alert>
-
-                <Alert status="success">
-                  <AlertIcon />
-                  Data uploaded to the server. Fire on!
-                </Alert>
-
-                <Alert status="warning">
-                  <AlertIcon />
-                  Seems your account is about expire, upgrade now
-                </Alert>
-
-                <Alert status="info">
-                  <AlertIcon />
-                  Chakra is going live on August 30th. Get ready!
-                </Alert>
               </Stack>
             </Box>
           </Stack>
