51d146b6bd05908a66daf9aa6f1c8d1f846ef93f
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2024-08-29 17:23:42 -0300

    update

--- src/app/(private_routers)/home/componentes/tabela/index.tsx
+++ src/app/(private_routers)/home/componentes/tabela/index.tsx
@@ -59,10 +59,13 @@ export const Tabela = ({ ClientData }: TabelaProps) => {
     const horaAgenda = item.fcweb?.hr_agenda?.split("T")[1].split(".")[0];
     const andamento = item.fcweb?.andamento;
     const statusPg = item.fcweb?.estatos_pgto;
-    console.log("🚀 ~ file: index.tsx:statusPg", item.distrato);
     const colors = !item.ativo
       ? "red.400"
       : item.distrato && user?.hierarquia === "ADM"
+      ? "gray.600"
+      : item.distrato && user?.hierarquia === "CONST"
+      ? "gray.600"
+      : item.distrato && user?.hierarquia === "GRT"
       ? "gray.600"
       : "transparent";
 
